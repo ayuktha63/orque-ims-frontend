1fdf60a404b13e6f58ff42a4e19f44eed05bc22a
nanakductcleaning20-dotcom <nanakductcleaning20@gmail.com>
2026-01-23 22:20:31 +0530

    breadcrumbs fixed and orque logo white

--- src/app/layout/topbar/topbar.ts
+++ src/app/layout/topbar/topbar.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatButtonModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './topbar.html',
   styleUrls: ['./topbar.css']
 })
